765a7905a6b1a39b1cc0a72ecfcd350b98677b8d
Grib <grib.ua@ukr.net>
2024-02-02 19:23:34 +0000

    save

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const balls = 12;
+const border = 0.01;
+const d = 0.5;
+
+for (let i = 1; i <= balls; i++){
+  radius = 4 / 3 * 3.14 * ((d *2 - border * 2)) ** 3 * i;
+console.log(`${balls}`)
+}
